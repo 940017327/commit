86fea106d49214eb1829f6b16138a6de835f1aa3
Administrator <940017327@qq.com>
2017-09-11 15:59:36 +0800

    首次提交代码

---
+++ app/src/main/java/com/example/administrator/bigmap/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.administrator.bigmap;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import java.io.IOException;
+
+public class MainActivity extends AppCompatActivity {
+
+    private MyView mView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initView();
+    }
+
+    private void initView() {
+        mView = (MyView) findViewById(R.id.mView);
+        try {
+            mView.setInputStream(getAssets().open("qm.jpg"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
